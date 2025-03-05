5898769b7fc00c2d593e75a45cccc1998594eab3
AIMRAN <lyaaimran@gmail.com>
2025-03-05 14:49:56 +0000

    modifier code

--- EX10.js
+++ EX10.js
@@ -1,6 +1,7 @@
 const readline=require('readline-sync');
-let  mote=readline.question("donner le mot tu veux convertir :");
-let motinvers= "";
-for(let i=mote.length-1;i>=0;i--){
-    motinvers+=mote[i];}
-console.log(motinvers);
+let mot=readline.question("donne le mots :");
+let motInverse='';
+for(let j=mot.length-1;0<=j;j--){
+    motInverse+=mot[j];
+}
+console.log("le mote inverser est ",motInverse);
